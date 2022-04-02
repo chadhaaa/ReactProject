059c06cdbffb111830cf373705492c41618ab834
Chadha HAJJI <chadha.hadji@gmail.com>
2022-04-02 17:29:27 +0100

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const app = require('./app')
+const app = express()
 const http = require('http')
 const mongoose = require('mongoose')
 
@@ -29,8 +29,19 @@ mongoose
 	.then(() => console.log('Connexion à MongoDB réussie !'))
 	.catch(() => console.log('Connexion à MongoDB échouée !'))
 
-const server = http.createServer(app)
-
-server.listen(8000, () => {
+app.listen(8000, () => {
 	console.log('Listening on port 8000')
 })
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+	)
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+	next()
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
