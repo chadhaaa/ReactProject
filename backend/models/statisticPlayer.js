a6c082c1908d88d72ea369ff73e7f92da1e56ac2
Chadha HAJJI <chadha.hadji@gmail.com>
2022-04-03 16:46:34 +0100

    some model changes

--- backend/models/statisticPlayer.js
+++ backend/models/statisticPlayer.js
@@ -1,6 +1,8 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const statPlayerSchema = mongoose.Schema({
-  statId: { type: mongoose.Schema.Types.ObjectId, ref: "Stats" },
-  playerId: { type: mongoose.Schema.Types.ObjectId, ref: "Player" },
-});
+	statId: { type: mongoose.Schema.Types.ObjectId, ref: 'Stats' },
+	playerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Player' },
+})
+
+module.exports = mongoose.model('statPlayer', statPlayerSchema)
