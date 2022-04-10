5e1366e2e838813dd9b26e4f2e58824ee93a041d
Chadha HAJJI <chadha.hadji@gmail.com>
2022-04-10 03:06:24 +0100

    FinalCRUDFront

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link, Switch } from 'react-router-dom'
+i
+import AddCompetence from './components/pages/crudCompetence/addComp'
+import GetCompetences from './components/pages/crudCompetence/getComp'
+import UpdateCompetence from './components/pages/crudCompetence/updateComp'
+import AddStatistic from './pages/crudStatistique/addStat'
+import GetStatistics from './pages/crudStatistique/getStat'
+import UpdateStatistic from './pages/crudStatistique/updateStat'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<Routes>
+					<Route exact path='/addCompetence' element={<AddCompetence />}></Route>
+
+					<Route exact path='/getCompetence' element={<GetCompetences />}></Route>
+
+					<Route exact path='/updateCompetence/:id' element={<UpdateCompetence />}></Route>
+
+					<Route exact path='/addStat' element={<AddStatistic />}></Route>
+
+					<Route exact path='/getStat' element={<GetStatistics />}></Route>
+
+					<Route exact path='/updateStat/:id' element={<UpdateStatistic />}></Route>
+				</Routes>
+			</div>
+		</Router>
+	)
 }
 
-export default App;
+export default App
