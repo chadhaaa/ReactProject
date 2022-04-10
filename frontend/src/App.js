3c992cbeb45585c4b492b73a3a7542b0c0313146
Chadha HAJJI <chadha.hadji@gmail.com>
2022-04-10 05:59:06 +0100

    ProfilePlayerFRONT

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from './logo.svg'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link, Switch } from 'react-router-dom'
+import GetProfile from './components/pages/profilePlayer/getProfile'
+import UpdateProfile from './components/pages/profilePlayer/updateProfile'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<Routes>
+					<Route exact path='/Profile/:id' element={<GetProfile />}></Route>
+
+					<Route exact path='/updateProfile/:id' element={<UpdateProfile />}></Route>
+				</Routes>
+			</div>
+		</Router>
+	)
 }
 
-export default App;
+export default App
