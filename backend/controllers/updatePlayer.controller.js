1acb064e8ee62cd4b57aac04a2c5ca445df712d0
Chadha HAJJI <chadha.hadji@gmail.com>
2022-05-03 13:43:01 +0100

    errors

--- backend/controllers/updatePlayer.controller.js
+++ backend/controllers/updatePlayer.controller.js
@@ -19,30 +19,30 @@ const UpdatePlayer = async (req, res) => {
 		},
 		{ new: true }
 	)
-	console.log('jhkjhkj', req.body.stats)
+	console.log('jhkjhkj', req.body.sessionPrice)
 	//update statPlayer
-	const statPlayer = await StatPlayer.deleteMany({
-		playerId: req.params.id,
-	})
-	Array.from(req.body.stats).forEach((stats) => {
-		const statsPlayer = new StatPlayer({
-			statId: stats,
-			playerId: player._id,
-		})
-		statsPlayer.save()
-	})
+	// const statPlayer = await StatPlayer.deleteMany({
+	// 	playerId: req.params.id,
+	// })
+	// Array.from(req.body.stats).forEach((stats) => {
+	// 	const statsPlayer = new StatPlayer({
+	// 		statId: stats,
+	// 		playerId: player._id,
+	// 	})
+	// 	statsPlayer.save()
+	// })
 
-	//update compPlayer
-	const compPlayer = await CompPlayer.deleteMany({
-		playerId: req.params.id,
-	})
-	Array.from(req.body.comp).forEach((comp) => {
-		const compPlayer = new CompPlayer({
-			compId: comp,
-			playerId: player._id,
-		})
-		compPlayer.save()
-	})
+	// //update compPlayer
+	// const compPlayer = await CompPlayer.deleteMany({
+	// 	playerId: req.params.id,
+	// })
+	// Array.from(req.body.comp).forEach((comp) => {
+	// 	const compPlayer = new CompPlayer({
+	// 		compId: comp,
+	// 		playerId: player._id,
+	// 	})
+	// 	compPlayer.save()
+	// })
 
 	if (!player) {
 		return res.status(404).send({ Message: 'ERROR ! ' })
@@ -50,27 +50,27 @@ const UpdatePlayer = async (req, res) => {
 	res.json(player)
 }
 
-const FindAllComp = async (req, res) => {
-	try {
-		const comp = await Comp.find()
+// const FindAllComp = async (req, res) => {
+// 	try {
+// 		const comp = await Comp.find()
 
-		res.json(comp)
-	} catch (err) {
-		res.send('Error ' + err)
-	}
-}
+// 		res.json(comp)
+// 	} catch (err) {
+// 		res.send('Error ' + err)
+// 	}
+// }
 
-const FindAllStats = async (req, res) => {
-	try {
-		const stats = await Stats.find().where(req.params.id, 'disciplineId')
-		res.json(stats)
-	} catch (err) {
-		res.send('Error ' + err)
-	}
-}
+// const FindAllStats = async (req, res) => {
+// 	try {
+// 		const stats = await Stats.find().where(req.params.id, 'disciplineId')
+// 		res.json(stats)
+// 	} catch (err) {
+// 		res.send('Error ' + err)
+// 	}
+// }
 
 module.exports = {
 	UpdatePlayer,
-	FindAllComp,
-	FindAllStats,
+	// FindAllComp,
+	// FindAllStats,
 }
--- backend/routes/updatePlayer.js
+++ backend/routes/updatePlayer.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 const {
 	UpdatePlayer,
-	FindAllComp,
-	FindAllStats,
+	// FindAllComp,
+	// FindAllStats,
 } = require('../controllers/updatePlayer.controller')
 
 router.put('/player/:id', UpdatePlayer)
-router.get('/com', FindAllComp)
-router.get('/stats/:id', FindAllStats)
+// router.get('/com', FindAllComp)
+// router.get('/stats/:id', FindAllStats)
 
 module.exports = router
