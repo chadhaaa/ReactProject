934c2211bc4f657e824f877f40ec69d874a80a5d
ihebslimen <ihebplt1@gmail.com>
2022-05-19 01:55:03 +0100

    na7it el marj

---
+++ backend/controllers/sessionFeedback.controller.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const Coach = require('../models/session')
+
+const UpdateSessionFeedback = async (req, res) => {
+	if (!mongoose.isValidObjectId(req.params.id)) {
+		res.status(400).send({ Message: 'Error: Competence ID invalid !' })
+	}
+	const session = await Coach.findOneAndUpdate(
+		{ _id: req.params.id },
+		{
+
+            day: req.body.day,
+            hour: req.body.hour,
+            cancellation: req.body.cancellation,
+            reason:  req.body.reason,
+            feedback:  req.body.reason,
+            idPlayer:  req.body.idPlayer,
+            idCoach:  req.body.idCoach,
+            idPlace:req.body.idPlace,
+            programId:req.body.programId,
+		},
+		{ new: true }
+	)
+
+
+	if (!session) {
+		return res.status(404).send({ Message: 'ERROR ! ' })
+	}
+	res.json(session)
+}
+
+
+
+module.exports = {
+	UpdateSessionFeedback,
+}
